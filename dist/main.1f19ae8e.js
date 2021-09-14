30bdbcce0cc8a722a767043bc84eb1cc9ed41b02
stela <stela.capsa@gmail.com>
2021-09-14 14:34:29 -0500

    mutiplying function

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -125,7 +125,14 @@ function myFunction(p1, p2) {
   return p1 * p2; // The function returns the product of p1 and p2
 }
 
-console.log(myFunction(2, 4)); // function counter() {
+console.log(myFunction(2, 4));
+var x = myFunction(4, 3); // Function is called, return value will end up in x
+
+console.log(x);
+
+function myFunction(a, b) {
+  return a * b; // Function returns the product of a and b
+} // function counter() {
 //   let seconds = 0;
 //   setInterval(() => {
 //     seconds += 1;
--- src/main.js
+++ src/main.js
@@ -23,6 +23,12 @@ function myFunction(p1, p2) {
 
 console.log(myFunction(2,4))
 
+let x = myFunction(4, 3);   // Function is called, return value will end up in x
+console.log(x)
+function myFunction(a, b) {
+  return a * b;             // Function returns the product of a and b
+}
+
 
 
 

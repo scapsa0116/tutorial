a96121174e69118cf4b34c9c07769bceb78f9752
stela <stela.capsa@gmail.com>
2021-09-14 15:34:57 -0500

    practice platform

--- src/main.js
+++ src/main.js
@@ -3,41 +3,36 @@
 
 
 
+// let 2firstName = "Kim"
+// console.log(2firstName)
 
 
 
 
 
-let firstName = "Kim"
-console.log(firstName)
+// function myFunction(p1, p2) {
+//   return p1 * p2;   // The function returns the product of p1 and p2
+// }
 
 
 
+// console.log(myFunction(2,4))
 
-
-function myFunction(p1, p2) {
-  return p1 * p2;   // The function returns the product of p1 and p2
-}
-
-
-
-console.log(myFunction(2,4))
-
-let x = myFunction(4, 3);   // Function is called, return value will end up in x
-console.log(x)
-function myFunction(a, b) {
-  return a * b;             // Function returns the product of a and b
-}
+// let x = myFunction(4, 3);   // Function is called, return value will end up in x
+// console.log(x)
+// function myFunction(a, b) {
+//   return a * b;             // Function returns the product of a and b
+// }
 
 
 
-function toCelsius(fahrenheit) {
-  return (5/9) * (fahrenheit-32);
-}
-document.getElementById("demo").innerHTML = toCelsius(77);
+// function toCelsius(fahrenheit) {
+//   return (5/9) * (fahrenheit-32);
+// }
+// document.getElementById("demo").innerHTML = toCelsius(77);
 
 
-console.log(toCelsius(1))
+// console.log(toCelsius(1))
 
 
 
